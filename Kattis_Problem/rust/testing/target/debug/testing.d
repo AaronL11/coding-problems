a10b7185a57fbb6e5f70b9a14c2a2af44af81e5e
AaronL11 <75177969+AaronL11@users.noreply.github.com>
2021-10-31 17:52:02 -0600

    Delete Kattis_Problem/rust/testing/target directory

--- Kattis_Problem/rust/testing/target/debug/testing.d
+++
@@ -1,1 +0,0 @@
-/home/aaron/Documents/Repos/coding-problems/Kattis_Problem/rust/testing/target/debug/testing: /home/aaron/Documents/Repos/coding-problems/Kattis_Problem/rust/testing/src/main.rs
--- Kattis_Problem/rust/testing/target/rls/debug/deps/testing-2f47a3132f9bed43.d
+++
@@ -1,5 +0,0 @@
-/home/aaron/Documents/Repos/coding-problems/Kattis_Problem/rust/testing/target/rls/debug/deps/testing-2f47a3132f9bed43.rmeta: src/main.rs
-
-/home/aaron/Documents/Repos/coding-problems/Kattis_Problem/rust/testing/target/rls/debug/deps/testing-2f47a3132f9bed43.d: src/main.rs
-
-src/main.rs:
