06803a07c7db275f1482a70d365b61c05d10159c
Aaron <aaron.lengyel@ucalgary.ca>
2023-12-10 02:18:33 -0700

    some meandering about

--- Kattis_Problem/python/main.py
+++ Kattis_Problem/python/main.py
@@ -1,8 +1,21 @@
-C = int(input())
-for _ in range(C):
-    arr = list(map(int,input().split()))
-    av = sum(arr)/len(arr)
-    print(len(arr))
-    print(sum(arr))
-    print(av)
-    print(f'{sum(x for x in arr if x >= av) / len(arr):.3f}%')
+from math import ceil
+
+def V(n, m, a, cache={}):
+    if (n,m) in cache:
+        return 0
+    if n <= a and m <= a:
+        cache[(n,m)] = 0
+        return 1
+    elif n <= a:
+        cache[(n,m)] = 0
+        return V(n,m-a,a) + 1
+    elif m <= a:
+        cache[(n,m)] = 0
+        return V(n-a,m,a) + 1
+    else:
+        cache[(n,m)] = 0
+        return V(n-a,m,a) + V(n,m-a,a) + 1
+
+n,m,a = map(int,input().split())
+print(V(n,m,a))
+print(ceil(n/a) * ceil(m/a))
--- Kattis_Problem/rust/files/src/main.rs
+++ Kattis_Problem/rust/files/src/main.rs
@@ -1,113 +1,149 @@
-use std::{str,error,fmt};
-use std::io;
-use io::{stdin,Stdin,BufRead,stdout,Stdout,BufWriter,Write};
-use std::fmt::{Display,Formatter};
+// use io::{stdin, stdout, BufRead, BufWriter, Stdin, Stdout, Write};
+// use std::collections::HashSet;
+// use std::fmt::{Display, Formatter};
+// use std::io;
+// use std::{error, fmt, str};
 
-#[derive(Debug,Default)]
-struct StopCode;
+// #[derive(Debug, Default)]
+// struct StopCode;
 
-impl error::Error for StopCode {}
+// impl error::Error for StopCode {}
 
-impl Display for StopCode {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "Code failed to execute")
-    }
-}
+// impl Display for StopCode {
+//     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+//         write!(f, "Code failed to execute")
+//     }
+// }
 
-impl From<io::Error> for StopCode {
-    fn from(_: io::Error) -> StopCode { StopCode }
-}
+// impl From<io::Error> for StopCode {
+//     fn from(_: io::Error) -> StopCode {
+//         StopCode
+//     }
+// }
 
-struct Scanner {
-    scanin: Stdin,
-    buffer: Vec<u8>,
-    iterator: str::SplitAsciiWhitespace<'static>,
-}
+// struct Scanner {
+//     scanin: Stdin,
+//     buffer: Vec<u8>,
+//     iterator: str::SplitAsciiWhitespace<'static>,
+// }
 
-#[allow(dead_code)]
-impl Scanner {
-    fn new() -> Self {
-        Self {
-            scanin: stdin(),
-            buffer: Vec::new(),
-            iterator: "".split_ascii_whitespace()
+// #[allow(dead_code)]
+// impl Scanner {
+//     fn new() -> Self {
+//         Self {
+//             scanin: stdin(),
+//             buffer: Vec::new(),
+//             iterator: "".split_ascii_whitespace(),
+//         }
+//     }
+//     fn next<T: str::FromStr>(&mut self) -> Result<T, StopCode> {
+//         loop {
+//             if let Some(input) = self.iterator.next() {
+//                 return input.parse::<T>().ok().ok_or(StopCode);
+//             }
+//             self.buffer.clear();
+//             self.scanin.lock().read_until(b'\n', &mut self.buffer)?;
+//             self.iterator = unsafe {
+//                 let slice = str::from_utf8_unchecked(&self.buffer);
+//                 std::mem::transmute(slice.split_ascii_whitespace())
+//             };
+//         }
+//     }
+//     fn take<T: str::FromStr>(&mut self, n: usize) -> Result<Vec<T>, StopCode> {
+//         let mut result = Vec::with_capacity(n);
+//         for _ in 0..n {
+//             let i = self.next::<T>()?;
+//             result.push(i);
+//         }
+//         Ok(result)
+//     }
+//     // Only works after const generics were stabilized!!
+//     //fn take<T, const N: usize>(&mut self) -> Result<[T;N],StopCode>
+//     //where
+//     //    T: str::FromStr + Default + Copy,
+//     //    [T;N]: Default {
+//     //    let mut result: [T;N] = Default::default();
+//     //    for i in 0..N {
+//     //        let n = self.next::<T>()?;
+//     //        result[i] = n;
+//     //    }
+//     //   Ok(result)
+//     //}
+//     fn take_tuple<T, V>(&mut self) -> Result<(T, V), StopCode>
+//     where
+//         T: str::FromStr,
+//         V: str::FromStr,
+//     {
+//         Ok((self.next::<T>()?, self.next::<V>()?))
+//     }
+//     fn take_tuple3<T, V, U>(&mut self) -> Result<(T, V, U), StopCode>
+//     where
+//         T: str::FromStr,
+//         V: str::FromStr,
+//         U: str::FromStr,
+//     {
+//         Ok((self.next::<T>()?, self.next::<V>()?, self.next::<U>()?))
+//     }
+// }
+
+// #[allow(non_snake_case)]
+// fn solve(mut scan: Scanner, mut out: BufWriter<Stdout>) -> Result<(), StopCode> {
+//     let t = scan.next::<usize>()?;
+//     let mut king = HashSet::new();
+//     let mut queen = HashSet::new();
+//     for _ in 0..t {
+//         king.clear();
+//         queen.clear();
+//         let (dx, dy) = scan.take_tuple::<i32, i32>()?;
+//         let (kx, ky) = scan.take_tuple::<i32, i32>()?;
+//         let (qx, qy) = scan.take_tuple::<i32, i32>()?;
+//         let diffs = [
+//             (dx, dy),
+//             (dy, dx),
+//             (dx, -dy),
+//             (-dx, dy),
+//             (dy, -dx),
+//             (-dy, dx),
+//             (-dx, -dy),
+//             (-dy, -dx),
+//         ];
+//         for (x, y) in diffs {
+//             king.insert((kx + x, ky + y));
+//             queen.insert((qx + x, qy + y));
+//         }
+//         writeln!(out, "{}", king.intersection(&queen).count())?;
+//     }
+//     Ok(out.flush()?)
+// }
+
+// fn main() -> Result<(), StopCode> {
+//     let scan = Scanner::new();
+//     let out = BufWriter::new(stdout());
+//     solve(scan, out)
+// }
+
+struct Solution;
+
+const EPS: f64 = 1E-7;
+
+impl Solution {
+    pub fn is_power_of_four(n: i32) -> bool {
+        if n == 1 {
+            true
+        } else if n <= 0 || n % 2 == 1 {
+            false
+        } else {
+            n == 4i32.pow(dbg!((n as f64).log(4.).round() as u32))
         }
     }
-    fn next<T: str::FromStr>(&mut self) -> Result<T, StopCode> {
-        loop {
-            if let Some(input) = self.iterator.next() {
-                return input.parse::<T>().ok().ok_or(StopCode)
-            }
-            self.buffer.clear();
-            self.scanin.lock().read_until(b'\n',&mut self.buffer)?;
-            self.iterator = unsafe {
-                let slice = str::from_utf8_unchecked(&self.buffer);
-                std::mem::transmute(slice.split_ascii_whitespace())
-            };
-        }
-    }
-    fn take<T: str::FromStr>(&mut self, n: usize) -> Result<Vec<T>,StopCode> {
-        let mut result = Vec::with_capacity(n);
-        for _ in 0..n {
-            let i = self.next::<T>()?;
-            result.push(i);
-        }
-        Ok(result)
-    }
-    // Only works after const generics were stabilized!!
-    //fn take<T, const N: usize>(&mut self) -> Result<[T;N],StopCode>
-    //where
-    //    T: str::FromStr + Default + Copy,
-    //    [T;N]: Default {
-    //    let mut result: [T;N] = Default::default();
-    //    for i in 0..N {
-    //        let n = self.next::<T>()?;
-    //        result[i] = n;
-    //    }
-    //   Ok(result) 
-    //}
-    fn take_tuple<T, V>(&mut self) -> Result<(T,V),StopCode>
-    where
-        T: str::FromStr,
-        V: str::FromStr {
-        Ok((
-            self.next::<T>()?,
-            self.next::<V>()?
-        ))
-    }
-    fn take_tuple3<T, V, U>(&mut self) -> Result<(T,V,U),StopCode>
-    where
-        T: str::FromStr,
-        V: str::FromStr,
-        U: str::FromStr {
-        Ok((
-            self.next::<T>()?,
-            self.next::<V>()?,
-            self.next::<U>()?
-        ))
-    }
 }
 
-#[allow(non_snake_case)]
-fn solve(
-    mut scan: Scanner,
-    mut out: BufWriter<Stdout>
-) -> Result<(), StopCode> {
-    loop {
-        let (n,m) = scan.take_tuple::<usize,usize>()?;
-        if (n,m) == (0,0) { break; }
-        let mut d =scan.take::<usize>(n)?;
-        d.sort_unstable();
-        let mut h =scan.take::<usize>(m)?;
-        h.sort_unstable();
-    }
-    Ok(out.flush()?)
+fn main() {
+    dbg!(Solution::is_power_of_four(16));
+    // let x = 5;
+    // println!("{} {}", x, Solution::preimage_size_fzf(x));
+    // println!("{} {}", x, trailing_zeroes(x))
+    // for x in (0..=100).step_by(5) {
+    //     println!("{} {}", x, Solution::preimage_size_fzf(x));
+    // }
 }
-
-fn main() -> Result<(), StopCode> {
-    let scan = Scanner::new();
-    let out = BufWriter::new(stdout());
-    solve(scan,out)
-}
-
-
